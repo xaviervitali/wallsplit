c0bf803b0ea1d499a7817a859e1b7d199883c9fa
Xavier Vitali <44967621+xaviervitali@users.noreply.github.com>
2026-05-24 19:06:45 +0200

    Initial Commit

---
+++ ImageSplitter/ImageSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSplitterApp.swift
+//  ImageSplitter
+//
+//  Created by Xavier Vitali on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ImageSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
